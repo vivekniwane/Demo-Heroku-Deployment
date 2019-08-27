5a48d8475a687d9890d0eb34574bb3da56f18ef2
viveknikam1 <47035774+viveknikam1@users.noreply.github.com>
2019-08-27 13:03:49 +0530

    Add files via upload

--- myapp.py
+++ myapp.py
@@ -10,8 +10,9 @@ def home():
 @app.route('/predict',methods=['POST'])
 def predict():
     model = pickle.load(open('model.pkl','rb'))
-    sal = model.predict(int(request.form['Experience']))
-    str1 = '<h1>Dear '+ request.form['Name'] +', your expected salary after '+ request.form['Experience'] +'Yrs Experience is '+ str(sal)+'LPA</h1>'
+    sal = model.predict(float(request.form['Experience']))
+    sal_final = str(round(sal[0],2))
+    str1 = '<h1>Dear '+ request.form['Name'] +', your expected salary after '+ request.form['Experience'] +'Yrs Experience is '+ sal_final+' LPA</h1>'
     return str1
 
 if __name__=='__main__':
